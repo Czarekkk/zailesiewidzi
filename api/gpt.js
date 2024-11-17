41c46d25b731bf3a105e25df0d2c53a9270ec745
Czarek Michalski <40615770+Czarekkk@users.noreply.github.com>
2024-11-17 21:41:28 +0100

    new flappy bird game and sounds

--- api/gpt.js
+++ api/gpt.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     // Handle CORS
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.zailesiewidzi.my');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
                 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
             },
             body: JSON.stringify({
-                model: "gpt-4o",
+                model: "gpt-4",
                 messages: [
                     {
                         "role": "system",
